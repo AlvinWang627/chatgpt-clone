38f9b50cc9ffe234f91398bd7c96e930ead08d3e
Alvin <AlvinWang627@gmail.com>
2024-03-12 16:22:13 +0800

    feat: install alert-dialog-ui

--- app.vue
+++ app.vue
@@ -1,7 +1,23 @@
 <template>
   <div>
     <NuxtWelcome />
-    <Button>123</Button>
+    <Button class="" :variant="'outline'" :size="'lg'">123</Button>
+    <AlertDialog>
+      <AlertDialogTrigger>Open</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   </div>
 </template>
 
